58a8c1da1a2af5ba93706d6bc7c7a73236aead93
OneDosh <admin@onedosh.com>
2025-06-17 12:15:46 +0100

    Updated onboarding.tsx

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -358,7 +358,7 @@ const styles = StyleSheet.create({
     paddingTop: 8,
   },
   subtitle: {
-    fontSize: isTablet ? 17 : 15,
+    fontSize: isTablet ? 20 : 19,
     fontFamily: 'SF-Pro-Text-Regular',
     color: '#848D97',
     lineHeight: isTablet ? 24 : 20,
